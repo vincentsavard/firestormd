5b281afc97fced7c04ca206c09ff946901aa7c2f
Vincent Savard <savard.vincent at gmail dot com>
2016-03-07 21:29:36 -0500

    Integrating Configuration with the project

--- firestormd/ui/services.py
+++ firestormd/ui/services.py
@@ -1,3 +1,6 @@
+import os
+
+from firestormd.config.configuration import Configuration
 from firestormd.media.mediaplayer import MediaPlayer
 from firestormd.media.mediafinder import MediaFinder
 from firestormd.media.mediadatabase import MemoryMediaDatabase
@@ -6,7 +9,20 @@ from firestormd.media.drivers.mplayer import MPlayerDriver
 from firestormd.service.mediaservice import MediaService
 
 _MEDIA_EXTENSIONS = [".avi", ".mp4"]
-_MEDIA_DIRECTORY = "/home/vincent/dev/projects/void/pyrite/pyrited/build/data"
+_CONFIG_FILE = ".firestormrc"
+
+def load_configuration():
+    directories_to_look_into = ["~"]
+
+    for directory in directories_to_look_into:
+        if os.path.exists(os.path.join(directory, _CONFIG_FILE)):
+            with open(os.path.join(directory, _CONFIG_FILE)) as file_handle:
+                return Configuration(file_handle.read())
+
+    return Configuration()
+
+_CONFIG = load_configuration()
+_MEDIA_DIRECTORY = _CONFIG["videos"]["directory"].value
 
 _media_database = MemoryMediaDatabase()
 _media_finder = MediaFinder(_MEDIA_DIRECTORY, _MEDIA_EXTENSIONS)
